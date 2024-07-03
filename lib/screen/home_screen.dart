ea1d4a8fde9935bc2305801fd7e4bf5d23045a0e
charles <rmswo417@naver.com>
2024-07-03 10:21:48 +0900

    일정 불러오기 데이터 베이스에 저장하기 위해 바텀시트 부분에 데이터를 가져와 저장하는 것까지 함

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,9 +3,11 @@ import 'package:calendar_scheduler/component/custom_text_field.dart';
 import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/const/color.dart';
+import 'package:calendar_scheduler/database/drift.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../component/schedule_card.dart';
@@ -58,12 +60,15 @@ class _HomeScreenState extends State<HomeScreen> {
           final schedule = await showModalBottomSheet<ScheduleTable>(
             context: context,
             builder: (_) {
-              return ScheduleBottomSheet(selectedDay: selectedDay);
+              return ScheduleBottomSheet(
+                selectedDay: selectedDay,
+              );
             },
           );
-          if (schedule == null) {
-            return;
-          }
+
+          setState(() {
+
+          });
           //
           // setState(
           //   () {
@@ -100,30 +105,50 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Padding(
                 padding:
                     const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-                child: ListView.separated(
-                  itemCount: 0,
-                  itemBuilder: (BuildContext context, int index) {
-                    ///선택된 날짜에 해당되는 일정 리스트로 저장
-                    ///List<Schedule>
+                child: FutureBuilder<List<ScheduleTableData>>(
+                    future: GetIt.I<AppDatabase>().getSchedules(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text(snapshot.error.toString()),
+                        );
+                      }
+                      if (!snapshot.hasData &&
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
 
+                      final schedules = snapshot.data!;
 
-                    return ScheduleCard(
-                        color: Color(
-                          int.parse(
-                            "FF000000",
-                            radix: 16,
-                          ),
-                        ),
-                        content: "test",
-                        startTime: 12,
-                        endTime: 14);
-                  },
-                  separatorBuilder: (BuildContext contex, int index) {
-                    return SizedBox(
-                      height: 16.0,
-                    );
-                  },
-                ),
+                      final selectedSchedules = schedules
+                          .where((e) => e.date.isAtSameMomentAs(selectedDay))
+                          .toList();
+                      return ListView.separated(
+                        itemCount: selectedSchedules.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          ///선택된 날짜에 해당되는 일정 리스트로 저장
+                          ///List<Schedule>
+                          final schedule = selectedSchedules[index];
+                          return ScheduleCard(
+                              color: Color(
+                                int.parse(
+                                  "FF${schedule.color}",
+                                  radix: 16,
+                                ),
+                              ),
+                              content: schedule.content,
+                              startTime: schedule.startTime,
+                              endTime: schedule.endTime);
+                        },
+                        separatorBuilder: (BuildContext contex, int index) {
+                          return SizedBox(
+                            height: 16.0,
+                          );
+                        },
+                      );
+                    }),
               ),
             ),
           ],
